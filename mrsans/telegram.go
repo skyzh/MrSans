ffa06ff6a3a7b99a52c9ad657af50438459e2da3
Alex Chi <iskyzh@gmail.com>
2020-02-11 10:23:44 +0800

    telegram bot initialize will stop when timeout

--- mrsans/telegram.go
+++ mrsans/telegram.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"context"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
-func InitializeTelegramBot() {
-	bot, err := tgbotapi.NewBotAPI(Config.telegram_bot_token)
-	if err != nil {
-		log.Fatal("failed to initialize bot API", err)
+func InitializeTelegramBot(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, time.Minute)
+	defer cancel()
+	c := make(chan struct{})
+	go func() {
+		bot, err := tgbotapi.NewBotAPI(Config.telegram_bot_token)
+		if err != nil {
+			log.Fatal("failed to initialize bot API", err)
+		}
+		log.Infof("authorized as account %s", bot.Self.UserName)
+		c <- struct{}{}
+	}()
+	select {
+	case <-ctx.Done():
+		log.Fatal("failed to initialize telegram bot: ", ctx.Err())
+	case <-c:
 	}
-	log.Infof("authorized as account %s", bot.Self.UserName)
 }
 
 func SensePushMessage(caption string, photo string) {
